ec4dc7c0d3347b144ebd2bd8e59ae8bdc6da91b1
Franco <francisco.iram97@gmail.com>
2019-11-06 00:21:35 +0000

    first commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+use CloudCreativity\LaravelJsonApi\Exceptions\HandlesErrors;
+use Neomerx\JsonApi\Exceptions\JsonApiException;
+
+class Handler extends ExceptionHandler
+{
+    use HandlesErrors;
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        JsonApiException::class,
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        if ($this->isJsonApi($request, $e)) {
+            return $this->renderJsonApi($request, $e);
+        }
+
+        return parent::render($request, $exception);
+    }
+
+    protected function prepareException(Exception $e)
+    {
+        if ($e instanceof JsonApiException) {
+            return $this->prepareJsonApiException($e);
+        }
+
+        return parent::prepareException($e);
+    }
+}
